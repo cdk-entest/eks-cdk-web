817597fd639186146669e4d6d4b7b881016bec3f
entest <hai@entest.io>
2022-12-27 03:21:48 +0000

    Initial commit

---
+++ test/cdk-eks-fargate.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CdkEksFargate from '../lib/cdk-eks-fargate-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cdk-eks-fargate-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CdkEksFargate.CdkEksFargateStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
